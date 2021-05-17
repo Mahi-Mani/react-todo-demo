2a814ebc5fae0928b2b42af3d67d013c2fca07a8
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2021-05-17 12:13:45 -0700

    Updates

---
+++ precache-manifest.df99606fbe107cf7d07af99e0a2d89b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465fb151c0d6bbc6b422eb744b51df5a",
+    "url": "/react-todo-demo/index.html"
+  },
+  {
+    "revision": "8265a6d924ef84689047",
+    "url": "/react-todo-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2cf6b9d126e0df03d2b0",
+    "url": "/react-todo-demo/static/js/2.152314b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-todo-demo/static/js/2.152314b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8265a6d924ef84689047",
+    "url": "/react-todo-demo/static/js/main.76ea674f.chunk.js"
+  },
+  {
+    "revision": "9af1839064020dfb9a7d",
+    "url": "/react-todo-demo/static/js/runtime-main.6ffb6712.js"
+  }
+]);
